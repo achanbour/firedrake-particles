e264e8ff7f77330750e5e4ca7e866f73a73c01e5
Anastasia Chanbour <anastasiachambour@gmail.com>
2026-04-01 15:37:37 +0100

    renamed test files

---
+++ test_linear_traj_diag_pos.py
@@ -0,0 +1,41 @@
+from firedrake import *
+import numpy as np
+from particle_traj_loop import move_particles_in_ref_space, BISECTION_COUNT
+
+"""
+Deterministic particle trajectory test using using constant velocity (identical for all particles)
+and particles initially placed on the mesh diagonal.
+"""
+
+# Define the parent mesh
+mesh = UnitSquareMesh(10, 10, quadrilateral=False)
+
+# Define the particle VOM
+# Place 10 particles at midpoints of the diagonal edges: 0.05, 0.15, ..., 0.95
+# These lie on y=x but avoid mesh vertices (which are at multiples of 0.1 for a 10x10 mesh)
+n_particles = 10
+x_diag = np.arange(n_particles) / 10.0 + 0.05
+x0 = np.column_stack([x_diag, x_diag])
+particle_vom = VertexOnlyMesh(mesh, x0)
+print("Initial particle positions: ", particle_vom.coordinates.dat.data_ro)
+
+# Assign per particle velocities
+V = VectorFunctionSpace(particle_vom, "DG", 0, dim=particle_vom.geometric_dimension)
+v = Function(V, name="particle_velocity")
+V_io = VectorFunctionSpace(particle_vom.input_ordering, "DG", 0, dim=particle_vom.geometric_dimension)
+v_io = Function(V_io, name="io_particle_velocity")
+
+v0 = np.array( [0.01, 0.02])
+v_io.dat.data_wo[:] = np.tile(v0, (n_particles, 1))
+v.interpolate(v_io)
+print("Initial particle velocities: ", v.dat.data_ro)
+
+# dt=0.1, t=2.5: 1 particle removed
+# dt=0.1, t=7.5: 2 particles removed
+T = 7.5
+dt = 0.1
+T_final, removed_particles = move_particles_in_ref_space(particle_vom, mesh, v, dt, T, t=0.0, plot=True)
+print("Final particle positions: ", particle_vom.coordinates.dat.data_ro)
+print("Removed particles: ", removed_particles)
+
+print("Number of bisection calls to resolve crossings: ", BISECTION_COUNT)
