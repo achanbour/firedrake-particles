07308a29f3bbb0be29e307dbd677d670d52fed7c
Anastasia Chanbour <anastasiachambour@gmail.com>
2026-04-17 10:13:05 +0100

    added new finding regarding the scaling of bisection error

--- test_bisection_convergence.py
+++ test_bisection_convergence.py
@@ -29,7 +29,7 @@ T = 1.5
 dt = 0.1
 
 # bisection_iters = [28, 30, 40, 50]
-time_tols = [1e-3, 1e-4, 1e-6, 1e-8, 1e-10, 1e-12]
+time_tols = [1e-3, 1e-4, 1e-6, 1e-8, 1e-10, 1e-12, 1e-14, 1e-16]
 
 errors = []
 bisection_calls = []
@@ -59,6 +59,7 @@ for tol in time_tols:
         particle_vom, mesh, v, dt, T, t=0.0,
         max_bisection_iters=max_iter,
         time_tol = tol,
+        bary_tol=1e-11,
         plot=False
     )
     
@@ -78,13 +79,19 @@ for tol, calls, err in zip(time_tols, bisection_calls, errors):
     print(f"{tol:>12.0e} {err:>14.6e} {calls:>16d}")
 
 # plt.semilogy(time_tols, errors)
-plt.loglog(time_tols, errors)
+plt.loglog(time_tols, errors, label="L2 Error")
+# ref = np.array(time_tols)
+# plt.loglog(ref, ref * (errors[-1]/time_tols[-1]), '--k', alpha=0.75, label="slope 1")
+# plt.legend()
 plt.xlabel("Time tol")
 plt.ylabel("L2 error")
 plt.title("Bisection convergence")
 plt.savefig("plots/bisection_error.png")
 
 # NOTE:
-# - When the bisection algorithm does converge, its error is of the order of at most time_tol.
+# - When bisection converges, the final error is of the order of at least time_tol but it also depends on bary_tol: 
+#   if bary_tol <= time_tol then the error is O(time_tol), 
+#   if bary_tol > time_tol then the error is O(bary_tol)
+#   so error = O(max(bary_tol, time_tol))
 # - The number of bisection iterations is only relevant up to convergence (error is the same once bisection converged).
 # - The error is expected to plateau once time_tol is small enough.
