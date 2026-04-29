fixed bug in the computation of v_ref and matched bisection convergence test

particle_traj_solver.py
@@ -52,14 +52,14 @@ class ParticleTrajectorySolver():
     def solve(self, t_start, t_end):
         particle_ids = np.arange(self.particle_vom.num_vertices())
         boundary_particles = []
-        outer_loop_iter = 0
+        self.outer_loop_iter = 0
         
         if self.plotter:
             self.plotter.setup(self.particle_vom, self.parent_mesh)
 
         while t_start < t_end - 1e-12:
-            outer_loop_iter += 1
-            self.logger.outer_loop(outer_loop_iter, t_start, self.stepper.dt, t_end, self.num_particles)
+            self.outer_loop_iter += 1
+            self.logger.outer_loop(self.outer_loop_iter, t_start, self.stepper.dt, t_end, self.num_particles)
 
             # Run the inner time step loop
             boundary_particles_current = self._run_inner_loop()
@@ -72,7 +72,7 @@ class ParticleTrajectorySolver():
             )
 
             self.logger.outer_summary(
-                outer_loop_iter,
+                self.outer_loop_iter,
                 self.inner_loop_iter,
                 self.particles_inner_loop_iter,
                 boundary_particles_current,

tests/test_bisection_convergence.py
@@ -27,13 +27,13 @@ x0 = np.column_stack([x_diag, x_diag])
 v0 = np.array([0.45, 0.25])
 
 # Stepper params
-dt = 0.1 # controls how many crossings occur
 t_start = 0
-t_end = 1.0 # controls how much error gets accumulated
+t_end = 1.5 # controls how much error gets accumulated
+dt = 0.1 # controls how many crossings occur
 
-bary_tol = 1e-9
-rel_time_tol = 0
+bary_tol = 1e-16
 abs_time_tols = [1e-3, 1e-4, 1e-6, 1e-8, 1e-10, 1e-12, 1e-14, 1e-16]
+rel_time_tol = 0
 
 errors = []
 bisection_calls = []
@@ -99,7 +99,9 @@ plt.loglog(abs_time_tols, errors, label="L2 Error")
 plt.xlabel("Absolute time tol")
 plt.ylabel("L2 error")
 plt.title("Bisection convergence")
-plt.savefig("plots/bisection_error.png")
+plot_dir = "./plots"
+os.makedirs(plot_dir, exist_ok=True)
+plt.savefig(f"{plot_dir}/bisection_error.png")
 
 # NOTE:
 # - When bisection converges, the final error is of the order of at least time_tol but it also depends on bary_tol: 

tests/test_linear_traj_diag_pos.py
@@ -14,8 +14,8 @@ Linear particle trajectory using constant velocity (identical for all particles)
 # Define the parent mesh
 parent_mesh = UnitSquareMesh(10, 10, quadrilateral=False)
 
-# Define the particle VOM
-n_particles = 10
+# Define the particles VOM
+num_particles = 10
 x_diag = np.arange(n_particles) / 10.0 + 0.05
 x0 = np.column_stack([x_diag, x_diag])
 particle_vom = VertexOnlyMesh(parent_mesh, x0)
@@ -29,7 +29,7 @@ V_io = VectorFunctionSpace(particle_vom.input_ordering, "DG", 0, dim=particle_vo
 v_io = Function(V_io, name="io_particle_velocity")
 
 v0 = np.array( [0.01, 0.02])
-v_io.dat.data_wo[:] = np.tile(v0, (n_particles, 1))
+v_io.dat.data_wo[:] = np.tile(v0, (num_particles, 1))
 v.interpolate(v_io)
 v0_vom = v.dat.data_ro.copy()
 print("Initial particle velocities: ", v0_vom)

tests/test_linear_traj_random_pos.py
@@ -1,11 +1,16 @@
 from firedrake import *
 from firedrake.petsc import PETSc
 import numpy as np
-from particle_traj_loop_old import solve_particle_traj_in_ref_space
+
+import sys, os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+
+from particle_time_stepper import ForwardEulerStepper
+from particle_crossing_solver import BisectionSolver, BisectionSolverParams
+from particle_traj_solver import ParticleTrajectorySolver, ParticleTrajectorySolverParams
 
 """
-Deterministic particle trajectory test with random constant velocity (different across particles)
-and random starting positions.
+Linear particle trajectory using constant velocity (identical for all particles) and random initial positions.
 """
 
 np.random.seed(42)
@@ -15,59 +20,69 @@ dt = 0.01 # time step
 T = 1.0
 
 # Define the parent mesh
-mesh = UnitSquareMesh(10, 10, quadrilateral=False)
-# mesh = PeriodicUnitSquareMesh(10, 10)
+parent_mesh = UnitSquareMesh(10, 10, quadrilateral=False)
 
-with PETSc.Log.Event("PreComputeCellFacetData"):
-    _ = mesh.topology.cell_facet_neighbours
-    _ = mesh.topology.cell_facet_coord_transforms
+_ = parent_mesh.topology.cell_facet_neighbours
+_ = parent_mesh.topology.cell_facet_coord_transforms
 
 # Define the particles VOM
-N = 10
-particle_coords = np.random.rand(N, 2)
-particle_vom = VertexOnlyMesh(mesh, particle_coords)
-initial_particle_coords = particle_vom.coordinates.dat.data_ro.copy()
-gdim = particle_vom.geometric_dimension
-print("Initial particle positions (in primary VOM order): ", particle_vom.coordinates.dat.data_ro)
+num_particles = 10
+particle_coords = np.random.rand(num_particles, 2)
+particle_vom = VertexOnlyMesh(parent_mesh, particle_coords)
+x0_vom = particle_vom.coordinates.dat.data_ro.copy()
+print("Initial particle positions (in VOM order): ", x0_vom)
 
-# Assign per-particle velocities
-V = VectorFunctionSpace(particle_vom, "DG", 0, dim=gdim)
-V_io = VectorFunctionSpace(particle_vom.input_ordering, "DG", 0, dim=gdim)
+# Define per-particle velocities
+V = VectorFunctionSpace(particle_vom, "DG", 0, dim=particle_vom.geometric_dimension)
+V_io = VectorFunctionSpace(particle_vom.input_ordering, "DG", 0, dim=particle_vom.geometric_dimension)
 v = Function(V)
 v_io = Function(V_io)
-input_velocities = np.random.normal(0.1, 0.5, size=(N,2)) # 0.01
+input_velocities = np.random.normal(0.1, 0.5, size=(num_particles,2))
 v_io.dat.data[:] = input_velocities
 v.interpolate(v_io)
-initial_particle_velocities = v.dat.data_ro.copy()
+v0_vom = v.dat.data_ro.copy()
+print("Initial particle velocities: ", v)
 
-# Move particles in ref. space
-# import timeit
+
+# Define solvers
+t_start = 0.0
+t_end = 1
+dt = 0.01
+stepper = ForwardEulerStepper(particle_vom, dt, v=v)
+
+bisection_params = BisectionSolverParams(max_iters=30)
+cell_crossing_solver = BisectionSolver(bisection_params)
+
+abs_time_tol = 1e-9
+bary_tol = 1e-9
+particle_traj_solver_params = ParticleTrajectorySolverParams(
+    bary_tol=bary_tol,
+    abs_time_tol=abs_time_tol,
+    rel_time_tol=0,
+    max_iters=50,
+    plot=False
+)
+particle_traj_solver = ParticleTrajectorySolver(stepper, cell_crossing_solver, particle_traj_solver_params)
+
 with PETSc.Log.Event("ParticleTrajectoryLoop"):
-    # t0 = timeit.default_timer()
-    T_final, removed_particles = solve_particle_traj_in_ref_space(particle_vom, mesh, v, dt, T, t=0.0, plot=True)
-    # t1 = timeit.default_timer()
-    # print(f"[wall_time] {t1 - t0} s")
+    T_final, removed_particles = particle_traj_solver.solve(t_start, t_end)
+
 print("Final particle positions: ", particle_vom.coordinates.dat.data_ro)
 print("Removed particles: ", removed_particles)
 
-from particle_traj_loop_old import BISECTION_COUNT
-print("Number of bisection calls to resolve crossings: ", BISECTION_COUNT)
+x_final_expected = x0_vom + T_final*v0_vom
 
-# from particle_time_stepper import STEP_COUNT
-# print("Total ForwardEulerTimeStepper calls: ", STEP_COUNT)
-
-# from pyop2.caching import print_cache_stats
-# print_cache_stats()
-# replace by: PYOP2_CACHE_INFO=1
-
-# exact_final_coords_io = particle_coords + T_final*input_velocities 
-# print("Exact final particle positions (IO): ", exact_final_coords_io)
-
-exact_final_coords = initial_particle_coords + T_final*initial_particle_velocities
-keep = np.ones(exact_final_coords.shape[0], dtype=bool)
+keep = np.ones(x_final_expected.shape[0], dtype=bool)
 keep[removed_particles] = False
-exact_final_coords_survived = exact_final_coords[keep]
+x_final_expected_survived = x_final_expected[keep]
 
-print("Exact final particle positions: ", exact_final_coords_survived)
-print("Exact results matched up to 1e-5: ", np.allclose(particle_vom.coordinates.dat.data_ro, exact_final_coords_survived, atol=1e-5, rtol=0))
-print("Exact results matched up to 1e-4: ", np.allclose(particle_vom.coordinates.dat.data_ro, exact_final_coords_survived, atol=1e-4, rtol=0))
+print("Exact final particle positions: ", x_final_expected_survived)
+print("Error: ", np.linalg.norm(x_final_expected_survived - particle_vom.coordinates.dat.data_ro, axis=1))
+
+# print(
+#     f"Exact results matched up to bary_tol={bary_tol}: \
+#     {np.allclose(particle_vom.coordinates.dat.data_ro, x_final_expected_survived, atol=bary_tol, rtol=0)}")
+
+# print(
+#     f"Exact results matched up to time_tol={abs_time_tol}: \
+#     {np.allclose(particle_vom.coordinates.dat.data_ro, x_final_expected_survived, atol=abs_time_tol, rtol=0)}")