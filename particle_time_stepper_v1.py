c13dc3349b841b1204c5123e03def3b9dc2b7dec
Anastasia Chanbour <anastasiachambour@gmail.com>
2026-05-13 13:49:33 +0100

    implemented and tested 3 different particle time steppers

--- particle_time_stepper_v1.py
+++ particle_time_stepper_v1.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-from functools import partial
 
 from firedrake.interpolation import interpolate, get_interpolator
 from firedrake.assemble import assemble
@@ -13,15 +12,14 @@ from ufl.operators import inv
 from ufl.core.expr import Expr
 from ufl.domain import extract_unique_domain
 
-
 class ParticleTimeStepper(ABC):
     """
     Abstract base class for time steppers that numerically integrate an ODE
-    describing a particle trajectory e.g., dx/dt = v.
+    describing a particle trajectory dx/dt = v.
     """
     def __init__(self, particle_vom, dt, v, **kwargs):
         # NOTE: kwargs contain stepper parameter e.g., tolerances for error refinement in adapative time stepping
-        # or Butcher tableau describing the integrator's parameters.
+        # or Butcher tableau for describing the integrator's parameters.
 
         # Validate the stepper's arguments
         if not isinstance(particle_vom.topology, VertexOnlyMeshTopology):
@@ -40,9 +38,7 @@ class ParticleTimeStepper(ABC):
         self._dt = dt
         FS = FunctionSpace(particle_vom, "DG", 0)
         self._dt_fn = Function(FS, name="per-particle-time-step-function")
-        
-        # Register fields owned by stepper
-        self._fields = [self._dt_fn] 
+        self._fields = [self._dt_fn] # register fields owned by the stepper
 
         
     @abstractmethod
@@ -65,7 +61,7 @@ class ParticleTimeStepper(ABC):
     def dt(self):
         return self._dt
     
-    # NOTE: Using private attributes + property methods enforce a read-only contract on fields that form the stepper's update expression.
+    # NOTE: Using private attributes + property methods enforce a read-only contract on fields owned by the stepper.
     # Re-assigning the fields with a new Function (e.g., stepper.X = ...) would silently invalidate the update expr and associated cached parloops.
     # If we want to allow users to swap out the fields then this should be done via an explicit setter method:
 
@@ -77,45 +73,44 @@ class ParticleTimeStepper(ABC):
 
 
     def _build_step_callable(self):
-        """Build and cache the step interpolation callable."""
+        """Build and cache the step interpolation callables."""
         interpolation_expr = interpolate(self._update_expr, self._X.function_space()) # symbolic interpolation expr
-        self.step_callable = partial(assemble, interpolation_expr)
+        interpolator = get_interpolator(interpolation_expr) # numerical interpolator
+        self.step_callable = interpolator._get_callable() # parloops
         self._step_callable_is_current = True
-
+    
 
     def _check_step_callable_is_current(self):
-        """Rebuild the step interpolation callable."""
+        """Trigger a rebuild of the step interpolation callables."""
         if not self._step_callable_is_current:
             self._build_step_callable()
 
 
-    def _rebuild_fields(self):
-        """Rebuild all fields after a VOM state change"""
-        for f in self._fields:
-            if isinstance(f, Function):
-                f._match_mesh_topology_version()
-            else:
-                raise TypeError("Cannot rebuild a field that is not a Function.")
-    
-    def _rebuild_exprs(self):
-        """Rebuild all expressions"""
+    def _reevaluate_fields(self):
+        """Re-evaluate all fields needed by step."""
         pass
 
 
+    def _rebuild_fields(self):
+        """Rebuild all fields eagerly after a change to the VOM's topology"""
+        for f in self._fields:
+            if isinstance(f, Function):
+                f._match_mesh_topology_version() # rebuilds the FS
+            else:
+                raise TypeError("Encountered a field in the stepper that is not a Function.")
+    
 
     # NOTE: To be removed once rebuild_vom and rebuild_function are fixed.
     def invalidate(self):
         """Mark all callables as stale."""
-        self._rebuild_fields() # mutates the fields (Functions) in places
-        self._rebuild_exprs() # produces a new symbolic expr hence need to re-assign the update expr
-        self._build_update_expr()
+        self._rebuild_fields()
         self._step_callable_is_current = False
 
 
     def step(self):
         # self._reevaluate_fields()
         self._check_step_callable_is_current()
-        result = self.step_callable() # assemble the cached interpolation
+        result = self.step_callable() # execute cached parloops
         return result
 
 
@@ -126,7 +121,7 @@ class ForwardEulerStepper(ParticleTimeStepper):
     X(t + dt) = X(t) + J^-1 * v * dt
 
     where J is the Jacobian of the geometric map from reference space to physical space F: X -> x
-    used to pull the velocity field back to reference space.
+    used to pullback the velocity field to reference space.
     """
 
     def __init__(self, particle_vom, dt, v):
@@ -135,30 +130,45 @@ class ForwardEulerStepper(ParticleTimeStepper):
         # Define any additional fields owned by the stepper
         x = SpatialCoordinate(self.particle_vom._parent_mesh)
         self._invJ_expr = inv(ReferenceGrad(x))
-        
-        self._v = v
 
+        self._v = v
         if extract_unique_domain(v) is self.particle_vom:
             # Appending self._v to self._fields ensures that it gets resized appropriately to match the new topology
             # so that the interpolation to self._v is correct
             self._fields.append(self._v)
 
-        # Define and store the symbolic expression for v_ref to be used in the update expression
-        # NOTE: The FS defined on the particle VOM may become stale when the particle VOM gets rebuilt!
-        # Registering the fields ensures their underlying FS get rebuilt, and therefore that the target space in the interpolation
-        # is always defined on the latest VOM.
+        # Option 1: Define and store the symbolic expression for v_ref and use that in the update expression
         if extract_unique_domain(self._v) is self.particle_vom:
             TFS = TensorFunctionSpace(self.particle_vom, "DG", 0)
-            # self._v_ref = interpolate(self._invJ_expr, TFS) * self._v
+
+            # NOTE: TFS becomes stale when particle_vom is rebuilt!
+            # Hence we register the fields so their FS get rebuilt.
             self.invJ_fn = Function(TFS)
             self._fields.append(self.invJ_fn)
+            # self._v_ref = interpolate(self._invJ_expr, TFS) * self._v
             self._v_ref = interpolate(self._invJ_expr, self.invJ_fn.function_space()) * self._v
         else:
             VFS = VectorFunctionSpace(self.particle_vom, "DG", 0)
-            # self._v_ref = interpolate(self._invJ_expr * self._v, VFS)
             self._v_ref_fn = Function(VFS)
             self._fields.append(self._v_ref_fn)
+            # self._v_ref = interpolate(self._invJ_expr, VFS) * self._v
             self._v_ref = interpolate(self._invJ_expr * self._v, self._v_ref_fn.function_space())
+
+        """
+        # Option 2: Define separate expressions for invJ and v and use their assembled output (Function) in the update expression
+        TFS = TensorFunctionSpace(self.particle_vom, "DG", 0)
+        self._invJ_fn = Function(TFS)
+        self._fields.append(self._invJ_fn)
+
+        VFS = VectorFunctionSpace(self.particle_vom, "DG", 0)
+        self._v_vom = Function(VFS)
+        self._fields.append(self._v_vom)
+
+        self._v_vom.interpolate(self._v)
+
+        # Both functions are on the VOM so the product is defined on a single domain.
+        self._v_ref = self._invJ_fn * self._v_vom
+        """
         
         self._build_update_expr()
         self._build_step_callable()
@@ -167,17 +177,35 @@ class ForwardEulerStepper(ParticleTimeStepper):
     def _build_update_expr(self):
         self._update_expr = self._X + self._v_ref * self._dt_fn
 
-    def _rebuild_exprs(self):
-        if extract_unique_domain(self._v) is self.particle_vom:
-            self._v_ref = interpolate(self._invJ_expr, self.invJ_fn.function_space()) * self._v
-        else:
-            self._v_ref = interpolate(self._invJ_expr * self._v, self._v_ref_fn.function_space())
 
+    def _reevaluate_fields(self):
+        self._invJ_fn.interpolate(self._invJ_expr)
+        self._v_vom.interpolate(self._v)
+    
     @property
     def v(self):
         return self._v
     
     @property
     def v_ref(self):
-        # TODO: Cache the result of from step and reuse here
-        return assemble(interpolate(self._v_ref, self.particle_vom.coordinates.function_space()))
+        # return assemble(interpolate(self._v_ref, self._v_vom.function_space()))
+        return assemble(interpolate(self._v_ref, self._v.function_space()))
+
+
+"""
+evaluating the update expr amounts to doing assemble(interpolate(update_expr, VOM)) where update expr is: 
+X + dt * v_ref where v_ref = invJ * v
+
+- invJ = inv(Reference(Grad(x))) lives on parent mesh
+- v may live on VOM or on parent mesh
+- so v_ref = interpolate(invJ * v, VOM) or interpolate(invJ, VOM) * v
+
+instead of assembling v_ref into a Function (which involves assembles two interpolations), keep v_ref symbolic in the update expr.
+
+currently fails in get_interpolator as extract_unique_domain raises an error
+
+interpolate(interpolate(invJ, VOM) * v, VFS) assembles into a Coefficient on the VOM
+
+Try splitting the operand using CoefficientSplitter(interpolate(invJ, VOM) * v) as is done in
+MixedInterpolator on forms using arguments.
+"""
